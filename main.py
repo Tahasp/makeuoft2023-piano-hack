82f42a8cfa8a5dc7a2dc055ebe257dc1639eb602
Tenner <tennercheung@gmail.com>
2023-02-18 17:46:15 -0500

    added iterator and input mapped to note output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mido
 from mido import MidiFile
 
+
 def list_select(selections: list[str], name: str):
     if len(set(selections)) == 1:
         return selections[0]
@@ -13,28 +14,26 @@ def list_select(selections: list[str], name: str):
 
 
 def main():
-
-
-
-    
-
     outputs: list[str] = mido.get_output_names()
     inputs: list[str] = mido.get_input_names()
-    # output = list_select(outputs, "midi output")
-    input = list_select(outputs, "midi input")
-    # output_port = mido.open_output(output)
-    input_port = mido.open_input(input)
+    output = list_select(outputs, "midi output")
+    input_dev = list_select(outputs, "midi input")
+    output_port = mido.open_output(output)
+    input_port = mido.open_input(input_dev)
+
+    # for msg in MidiFile('for_elise_by_beethoven.mid').play():
+    #     print(msg)
+    #     input()
+
+    myiter = iter(MidiFile('for_elise_by_beethoven.mid').play())
     for msg in input_port:
-        print(msg)
-
-
-    for msg in MidiFile('for_elise_by_beethoven.mid').play(): #chops song into notes
-            
-        msg.time = 0
-        print(msg.bytes())
-        output_port.send(msg)
-                
+        print("input:", msg)
+        outmsg = next(myiter)
         
-    
+        outmsg.time = 0
+        output_port.send(outmsg)
+        print("output:", outmsg)
+
+
 if __name__ == "__main__":
     main()
